7c5aa3700ecdb09c368cbc37c05f6add39ddff8e
S <seerla235@gmail.com>
2026-09-15 14:48:56 -0400

    Complete SCV data checker CLI

--- class2_data_checker.py
+++ class2_data_checker.py
@@ -19,3 +19,56 @@ def check_data(filename):
             missing_rows.append(row_number)
 
     return header, data, missing_rows
+
+# TODO 1: Create an ArgumentParser
+parser = argparse.ArgumentParser(
+    description="Check the quality of a CSV file."
+)
+
+
+# TODO 2: Add a named argument (required):
+parser.add_argument(
+    "--input", "-i",
+    required=True,
+    help="CSV file to check"
+)
+
+# TODO 3: Add an named argument (optional):
+parser.add_argument(
+    "--output", "-o",
+    default="data_quality.txt",
+    help="Output report filename"
+)
+
+# TODO 4: Add a boolean flag:
+# Long form: --verbose
+# Short form: -v
+# Use action="store_true"
+# Help: "Show detailed DEBUG messages"
+
+parser.add_argument(
+    "--verbose", "-v",
+    action="store_true",
+    help="show detailed DEBUG messages"
+)
+
+# TODO 5: Parse the command-line arguments
+
+args = parser.parse_args()
+
+# Check if the file exists 
+p = Path(args.input)
+if not p.is_file():
+    print(f"File not found: '{args.input}'")
+    sys.exit(1)
+
+print(f"File validated: '{args.input}'")
+
+# Check the data
+header, data, missing_rows = check_data(args.input)
+
+# Save the report
+with open(args.output, "w") as f:
+    f.write(f"Number of rows: {len(data)}\n")
+    f.write(f"Number of columns: {len(header)}\n")
+    f.write(f"Number of rows with missing values: {len(missing_rows)}\n")
