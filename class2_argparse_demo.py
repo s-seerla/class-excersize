632ca26ef25de53ebacec7539229fe0e3227c6f8
S <seerla235@gmail.com>
2026-09-18 13:59:18 -0400

    Add .gitignore

---
+++ class2_argparse_demo.py
@@ -0,0 +1,23 @@
+import argparse
+
+imput_data = "sales.csv"
+pd.read_csv(input_data)
+
+parser = argparse.ArgumentParser(
+    description="Analyze a data file")
+
+parser.add_argument(
+    "--input", "-i",
+    required=True,
+    help="Path to input CSV file")
+
+parser.add_argument(
+    "--output", "-o",
+    default="results.txt",
+    help="Output file path")
+
+parser.add_argument(
+    "--verbose",
+    "-v", 
+    action="store_true", 
+    help="Print detailed information")
